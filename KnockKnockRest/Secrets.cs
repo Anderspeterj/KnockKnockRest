0f2f9cac6973954a9a4069baa6a57172ab1f41f9
Datamagikeren <lasse4040@gmail.com>
2023-05-09 09:45:16 +0200

    remove secrets

--- KnockKnockRest/Secrets.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace KnockKnockRest
-{
-    public class Secrets
-    {
-        public static readonly string ConnectionString = "Dette skal ikke være her stadig ikke  og slet ikke nu";
-    }
-}
