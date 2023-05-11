c76acbf46e56bd09cfd9da3d14f417ce873c02cb
Datamagikeren <lasse4040@gmail.com>
2023-05-11 15:49:22 +0200

    add db string

--- KnockKnockRest/Secrets.cs
+++ KnockKnockRest/Secrets.cs
@@ -3,7 +3,8 @@
     public class Secrets
     {
 
-        public static readonly string ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=KnockKnockLocalDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        //public static readonly string ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=KnockKnockLocalDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"; // localdb
         //public static readonly string ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ReceiptShareDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        public static readonly string ConnectionString = "Data Source=knockknockserver1.database.windows.net;Initial Catalog=knockknockdb1;User ID=knockknockadmin;Password=Pepsimax123;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
     }
 }
